b65d33d1ffb813cb8e93f8936674ddf7fb0b34a9
AhuekweOgochukwuMercy <ahuekwe.o.mercy@gmail.com>
2022-07-07 03:48:38 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - a function that prints 10 times the alphabet
+*
+* Return: x10 a-z
+*/
+void print_alphabet_x10(void);
+{
+	int n, co;
+
+	co = o;
+
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; ntt)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
+	}
+}
