ed5f9e69267255ef7c091dc2b1f54ccea5aa6a79
AhuekweOgochukwuMercy <ahuekwe.o.mercy@gmail.com>
2022-07-09 14:48:00 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line -> printing line
+ * @n: integer params
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		 _putchar(95);
+		_putchar('\n');
+	}
+}
