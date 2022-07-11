27304ad9386d345d6c2a7428fe435a6de788505e
AhuekweOgochukwuMercy <ahuekwe.o.mercy@gmail.com>
2022-07-11 01:54:30 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,6 +7,6 @@ void positive_or_negative(int i);
 int largest_numbe(int a, int b, int c);
 int convert_day(int month,int day);
 void print_remaining_days(int month,int day,int year);
-void print-to_98(int n);
+void print_to_98(int n);
 
 #endif
