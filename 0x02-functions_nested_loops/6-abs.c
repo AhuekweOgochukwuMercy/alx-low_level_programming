e50701158944fa352432a3fcfaf0f7eddbe4689c
AhuekweOgochukwuMercy <ahuekwe.o.mercy@gmail.com>
2022-07-13 08:05:29 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value.
+ * @b: The character to print
+ * Return: Always b
+ */
+int _abs(int b)
+{
+	if (b >= 0)
+		return (b);
+
+	else
+		return (-b);
+
+}
