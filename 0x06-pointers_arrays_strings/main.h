585ff7cf4f6c7593e3121dd76af873acb05d6f39
AhuekweOgochukwuMercy <ahuekwe.o.mercy@gmail.com>
2022-07-14 05:15:49 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
  * File: main.h
  * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
- * used in the 0x06-pointers_arrays_strings directory.
+ * Used in the 0x06-pointers_arrays_strings directory.
  */
 
 #include <stddef.h>
