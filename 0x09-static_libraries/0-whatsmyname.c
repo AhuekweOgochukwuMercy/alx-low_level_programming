8e8b5c0f42ca1aa11e7ea62249c4976ba828038f
AhuekweOgochukwuMercy <ahuekwe.o.mercy@gmail.com>
2022-07-24 05:09:01 -0700

    _putchar.c

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - function with two arguments
+ * @argc: int type argument
+ * @argv:cchar type pointer array argument
+ *
+ * Description:prints all arguments it receives
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%\n", argv[0]);
+	return (0);
+}
