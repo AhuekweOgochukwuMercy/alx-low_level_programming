0faa1ada9bf351c71bcdcf0cbad83e22ee7aa54f
AhuekweOgochukwuMercy <ahuekwe.o.mercy@gmail.com>
2022-07-28 15:43:33 -0700

    main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _error(void);
+int _isnumber(char *str);
+int _strlen(char *s);
+char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min, int max);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int _putchar(char c);
+
+#endif
